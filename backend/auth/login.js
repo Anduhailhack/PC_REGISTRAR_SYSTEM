3dd8c80c154c1b6a783d8fa6273441c4679d6bec
Anduhail hack <anduhailhack24@gmail.com>
2023-01-02 00:12:23 +0300

    Authentication process into considaration!

---
+++ backend/auth/login.js
@@ -0,0 +1,7 @@
+//TODO: Login stuff goes here 
+
+//1 - accept a post request with username and password 
+
+//2 - Suppozidly authenticate the username and password with database and ...
+
+//3 - Return a token of some sort for a feature api requests
