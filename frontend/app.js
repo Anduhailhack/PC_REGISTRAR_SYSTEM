2c2009f81d40c3c924c74db7361b6c8ec3b78220
Israel <israleanteneh@gmail.com>
2023-01-05 09:20:28 +0300

    new html and css file added

---
+++ frontend/app.js
@@ -0,0 +1,36 @@
+// let uinput = document.getElementById("U-input");
+// let qroutput = document.getElementById("QR-output")
+
+// let qrbtn = document.getElementById('QR-btns');
+// qrbtn.addEventListener('click', () =>{
+//     let uva = uinput.value;
+//     qroutput.innerHTML =""
+//     new QRCode(qroutput ,{
+//         text:uva,
+//         width:200,
+//         height:200, 
+//         colorlight:"red"
+//     })
+//     console.log
+// })
+
+
+// // const userinput=document.getElementById("user-input")
+   
+// // let btn1 =document.getElementById('btn')
+// // let qroutput =document.getElementById('QR-output')
+// // btn1.addEventListener('click', ()=>{
+// //     const inputv= userinput.value
+// //     qroutput.innerHTML =""
+// //     new QRCode(qroutput ,{
+// //         text:inputv,
+// //         width:200,
+// //         height:200, 
+// //         colorlight:"green"
+
+// //     })
+// // })
+let qrbtn = document.getElementById('QR-btns');
+ qrbtn.addEventListener('click', () =>{
+console.log("hello")
+ })
